024af176fac19b02c00bb068ac592cb964872912
Aniket Yadav <aniketyadav690@gmail.com>
2024-08-08 03:10:13 +0530

    update: images of recentOrders

--- src/components/RecentOrders.jsx
+++ src/components/RecentOrders.jsx
@@ -6,43 +6,43 @@ const orders = [
     orderNo: "15478256",
     amount: "$124.00",
     status: "Delivered",
-    image:
-      "https://www.photoshopessentials.com/newsite/wp-content/uploads/2018/08/resize-images-print-photoshop-f.jpg", 
+    image: "https://raajratna.com/wp-content/uploads/2019/01/person1.jpg",
   },
   {
     customer: "Jane Cooper",
     orderNo: "48965786",
     amount: "$305.02",
     status: "Delivered",
-    image: "https://via.placeholder.com/40", 
+    image: "https://raajratna.com/wp-content/uploads/2019/01/person2.jpg",
   },
   {
     customer: "Guy Hawkins",
     orderNo: "78958215",
     amount: "$45.88",
     status: "Cancelled",
-    image: "https://via.placeholder.com/40", 
+    image: "https://raajratna.com/wp-content/uploads/2019/01/person3.jpg",
   },
   {
     customer: "Kristin Watson",
     orderNo: "20965732",
     amount: "$60.00",
     status: "Pending",
-    image: "https://via.placeholder.com/40", 
+    image: "https://raajratna.com/wp-content/uploads/2019/01/person5.jpg",
   },
   {
     customer: "Cody Fisher",
     orderNo: "95715620",
     amount: "$545.00",
     status: "Delivered",
-    image: "https://via.placeholder.com/40", 
+    image: "https://raajratna.com/wp-content/uploads/2019/01/person6.jpg",
   },
   {
     customer: "Savannah Nguyen",
     orderNo: "78514568",
     amount: "$128.20",
     status: "Delivered",
-    image: "https://via.placeholder.com/40", 
+    image:
+      "https://as2.ftcdn.net/v2/jpg/03/83/25/83/1000_F_383258331_D8imaEMl8Q3lf7EKU2Pi78Cn0R7KkW9o.jpg",
   },
 ];
 
