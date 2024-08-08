af7d800a31faae34c16b57506cf6f2596e997cff
Aniket Yadav <aniketyadav690@gmail.com>
2024-08-08 21:21:39 +0530

    update: README file

--- src/components/DashboardContent.jsx
+++ src/components/DashboardContent.jsx
@@ -11,6 +11,7 @@ import {
 const DashboardContent = () => {
   return (
     <div className="flex flex-col p-4 space-y-4">
+      <h3 className="hidden text-lg font-bold lg:block">Dashboard</h3>
       <div className="flex flex-col items-stretch gap-4 items lg:flex-row">
         <OverviewCards />
         <NetProfit />
